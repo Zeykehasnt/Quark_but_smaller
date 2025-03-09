157163f3551425969d54e11bc3d2f7fc5b837ce0
MehVahdJukaar <matteogozzi1998@gmail.com>
2025-03-09 03:16:39 +0100

    fixed lootr mixin. still doesnt work tho..

--- src/main/java/org/violetmoon/quark/integration/mixin/lootr/ConfigManagerMixin.java
+++ src/main/java/org/violetmoon/quark/integration/mixin/lootr/ConfigManagerMixin.java
@@ -1,37 +1,36 @@
 package org.violetmoon.quark.integration.mixin.lootr;
 
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import noobanidus.mods.lootr.config.ConfigManager;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.violetmoon.quark.base.Quark;
 
-import java.util.Map;
+import java.util.HashMap;
 
-@Pseudo
 @Mixin(value = ConfigManager.class, remap = false)
 public class ConfigManagerMixin {
-	@ModifyVariable(method = "addSafeReplacement", at = @At("HEAD"), argsOnly = true, remap = false)
-	private static Block replacement(Block original, ResourceLocation location) {
-		Block block = BuiltInRegistries.BLOCK.get(location);
-		if(block != Blocks.AIR) {
-			Block lootrVariant = Quark.LOOTR_INTEGRATION.lootrVariant(block);
-			if(lootrVariant != null)
-				return lootrVariant;
-		}
 
-		return original;
-	}
+    @ModifyVariable(method = "addSafeReplacement", at = @At("HEAD"), argsOnly = true, remap = false)
+    private static Block replacement(Block original, ResourceLocation location) {
+        Block block = BuiltInRegistries.BLOCK.get(location);
+        if (block != Blocks.AIR) {
+            Block lootrVariant = Quark.LOOTR_INTEGRATION.lootrVariant(block);
+            if (lootrVariant != null)
+                return lootrVariant;
+        }
 
-	@ModifyReturnValue(method = "replacement", at = @At(value = "NEW", target = "java/util/HashMap", remap = false), remap = false)
-	private static Map<Block, Block> addQuarkChests(Map<Block, Block> original) {
-		Quark.LOOTR_INTEGRATION.populate(original);
-		return original;
-	}
+        return original;
+    }
+
+    @ModifyExpressionValue(method = "replacement", at = @At(value = "NEW", target = "java/util/HashMap", remap = false), remap = false)
+    private static HashMap addQuarkChests(HashMap original) {
+        Quark.LOOTR_INTEGRATION.populate(original);
+        return original;
+    }
 }
